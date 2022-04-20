1f33f2e9759db73655cc0220bbcb33795b98c28b
Bodescu Anamaria <bodescu.anamariia@gmail.com>
2022-04-20 16:09:30 +0300

    feat: display first job

---
+++ .history/src/App_20220420150804.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { FaAngleDoubleRight } from "react-icons/fa";
+const url = "https://course-api.com/react-tabs-project";
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
